c34e65a575898472b7b406f323af372d30da0961
arun gaikwad <arun8881212@gmail.com>
2026-06-03 15:32:03 +0530

    this is the latest change

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-console.log("Hello Arun");
-console.log("GitHub Actions Working");
+const http = require("http");
+
+http.createServer((req,res)=>{
+    res.write("Hello from Docker Container");
+    res.end();
+}).listen(3000);
+
+console.log("Server Started");
