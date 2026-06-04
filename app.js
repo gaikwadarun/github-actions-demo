d3d70188d5f29237a20694538b9e57574211f376
gaikwadarun <118682233+gaikwadarun@users.noreply.github.com>
2026-06-04 11:04:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 http.createServer((req,res)=>{
-    res.write("Hello from Docker Container");
+    res.write("Hello from Docker Container made by devops engineer");
     res.end();
 }).listen(3000);
 
